381d17d51be993823573d0543e0658808db5d26f
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-15 11:32:00 +0900

    main.cppでUseChStdをインクルードするように修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 
+#include"main.h"
+
+//挙動を確認したい項目のコメントアウトを外してください//
+
+////ChStdの使用方法//
+#include"UseChStd.h"
+
 
 int main(int _argc, char* _args[])
 {
-
-
-
-
-
-	return 0;
+	return mainFunction();
 }
