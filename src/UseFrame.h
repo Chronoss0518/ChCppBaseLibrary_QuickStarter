0378ccd7a208f78d550d3f61d76c1a1690078316
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-15 16:59:54 +0900

    BaseFrame及びFrameListの利用方法のサンプルを追加

---
+++ src/UseFrame.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include"main.h"
+
+//前準備としてBaseFrameを継承したクラスを用意
+class TestFrame1 : public ChCpp::BaseFrame
+{
+public:
+
+	void Update()override
+	{
+		system("cls");
+		std::cout << "run frame1" << std::endl;
+
+		//次のFrameをセットする//
+		ChangeFrame(1);
+	}
+};
+
+class TestFrame2 : public ChCpp::BaseFrame
+{
+public:
+
+	void Update()override
+	{
+		system("cls");
+		std::cout << "run frame2" << std::endl;
+
+		//次のFrameをセットする//
+		ChangeFrame(0);
+	}
+};
+
+inline int mainFunction()
+{
+	//BaseFrameは各シーンや画面を管理するためのクラスです//
+	//ぶっちゃけて言うと、利用しなくてもアプリは作れます//
+
+	//Frameを管理するオブジェクトを準備//
+	ChCpp::FrameList frameList;
+
+	//利用するフレームをセット//
+	frameList.SetFrame<TestFrame1>();
+	frameList.SetFrame<TestFrame2>();
+
+	int num = 0;
+	while (num != 'e' && num != 'E')
+	{
+		//Frameの更新及びFrameの切り替えを実施//
+		frameList.Update();
+		num = getchar();
+	}
+
+	return 0;
+}
