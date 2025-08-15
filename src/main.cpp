df43e8ec406d074efb4a3ced5e7677769c069774
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-15 16:40:25 +0900

    mainに追加したサンプルをインクルードできるように修正

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,14 @@
 ////ChStdの使用方法//
 //#include"UseChStd.h"
 
+////ChPtrの使用方法//
+//#include"UseChPtr.h"
+
 ////ChMath及びChMath3Dの利用方法//
-#include"UseChMathAndChMath3D.h"
+//#include"UseChMathAndChMath3D.h"
+
+////BitBoolの利用方法//
+//#include"UseBitBool.h"
 
 int main(int _argc, char* _args[])
 {
