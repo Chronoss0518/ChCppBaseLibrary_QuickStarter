79fd7dfa43c9758b66ecd4de458b329134df79f6
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-15 11:31:23 +0900

    ChStdの簡単な利用方法のサンプルを追加

---
+++ src/UseChStd.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include"main.h"
+
+enum class CastTest :unsigned char
+{
+	A,
+	B,
+	C
+};
+
+struct StructTest
+{
+	int num;
+	char str[50];
+	unsigned long value;
+};
+
+inline int mainFunction()
+{
+	//ChStdは全体で利用する便利機能を入れている名前空間です//
+
+	//EnumCastはenumまたはenum classの継承クラス(CastTestの場合はunsigned char)の値を取り出します。//
+	auto&& castTestValue = ChStd::EnumCast(CastTest::A);
+
+	//DecimalNumberToBaseNumberは入力した10進数の数値を対象の進数文字列へ変換します。//
+	//この例では8進数のchar型文字列へ変換します。//
+	auto&& number = ChStd::DecimalNumberToBaseNumber<char>(100, ChStd::OCTAL<char>());
+
+	//MCopyやMZeroはメモリの内容をコピーまたは0にしてくれる機能です//
+	//この例ではMZeroを用いて、値を全て0にしています//
+	StructTest structTest;
+	ChStd::MZero(&structTest);
+
+	return 0;
+}
