86d194941774f3b000420b77009c664433d7c8ae
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-15 11:42:16 +0900

    出力した各値をコマンドライン出力

--- src/UseChStd.h
+++ src/UseChStd.h
@@ -22,15 +22,23 @@ inline int mainFunction()
 
 	//EnumCastはenumまたはenum classの継承クラス(CastTestの場合はunsigned char)の値を取り出します。//
 	auto&& castTestValue = ChStd::EnumCast(CastTest::A);
+	
+	std::cout << (int)castTestValue << std::endl;
 
 	//DecimalNumberToBaseNumberは入力した10進数の数値を対象の進数文字列へ変換します。//
 	//この例では8進数のchar型文字列へ変換します。//
 	auto&& number = ChStd::DecimalNumberToBaseNumber<char>(100, ChStd::OCTAL<char>());
 
+	std::cout << number << std::endl;
+
 	//MCopyやMZeroはメモリの内容をコピーまたは0にしてくれる機能です//
 	//この例ではMZeroを用いて、値を全て0にしています//
 	StructTest structTest;
 	ChStd::MZero(&structTest);
+
+	std::cout <<"num [" << structTest.num << "]" << std::endl;
+	std::cout <<"str [" << structTest.str << "]" << std::endl;
+	std::cout <<"value [" << structTest.value << "]" << std::endl;
 
 	return 0;
 }
